69790e5e0702d5999a468ec1eca7aa97326d26e1
takiguchi <alexandre.pupim360@gmail.com>
2024-09-27 22:26:24 -0300

    update 27/09/24

--- POO/p1/1_empregada.php
+++ POO/p1/1_empregada.php
@@ -1,8 +1,58 @@
 <?php
 
-class empregado{
-    private string $nome;
+class Empregado{
+    private string $name;
     private string $sobrenome;
-    private string $setor; 
-    private int $salario_mensal;
+    private string $setor;
+    private int $salario;
+
+
+    public function setName(string $name):void{
+        $this->name = $name;
+    }
+    public function setSobrenome(string $sobrenome):void{
+        $this->sobrenome = $sobrenome;
+    }
+    public function setSetor(string $setor):void{
+        $this->setor = $setor;
+    }
+    public function setSalario(int $salario):void{
+        if($salario < 0){
+            $this->salario = 0;
+        }
+        else{
+            $this->salario = $salario;
+        }
+    }
+
+    public function getName():string{
+        return $this->name;
+    }
+    public function getSobrenome():string{
+        return $this->sobrenome;
+    }
+    public function getSetor():string{
+        return $this->setor;
+    }
+    public function getSalario():int{
+        return $this->salario;
+    }
+
+    public function salarioanual():int{
+        return $this->salario*12;
+    }
+
+    public function Infos():void{
+        echo"Empregado: {$this-> name} {$this->sobrenome}<hr>";
+        echo"Setor: {$this->setor}<hr>";
+        echo"Salario: {$this->salario}<hr>";
+        echo"Salario Anual: {$this->salarioanual()}<hr>";
 }
+}
+
+$empregado = new Empregado();
+$empregado->setName("Fern");
+$empregado->setSobrenome("Takiguchi");
+$empregado->setSetor("RH");
+$empregado->setSalario("3500");
+$empregado->Infos();
--- POO/p1/2_aluno.php
+++ POO/p1/2_aluno.php
@@ -7,4 +7,55 @@ class aluno{
     private string $disciplina;
     private float $nota_p1;
     private float $nota_p2;
+
+
+    public function __construct($nome,$ra,$curso,$disciplina,$nota_p1,$nota_p2){
+        $this-> nome = $nome;
+        $this-> ra = $ra;
+        $this-> curso = $curso; 
+        $this-> disciplina = $disciplina;
+        $this-> nota_p1 = $nota_p1;
+        $this-> nota_p2 = $nota_p2;
+    }
+
+    public function setNome(string $name):string{
+        $this->nome = $name;
+    }
+    public function setRa(int $ra):int{
+        $this->ra = $ra;
+    }
+    public function setCurso(string $curso):string{
+        $this->curso = $curso;
+    }
+    public function setDisciplina(string $disciplina):string{
+        $this-> disciplina = $disciplina;
+    }
+    public function setNota_p1(float $nota_p1):float{
+        $this->nota_p1 = $nota_p1;
+    }
+    public function setNota_p2(float $nota_p2):float{
+        $this->nota_p2 = $nota_p2;
+    }
+
+    public function getNome():string{
+        return $this-> nome;
+    }
+    public function getRa():int{
+        return $this-> ra;
+    }
+    public function getCurso():string{
+        return $this-> curso;
+    }
+    public function getDisciplina():string{
+        return $this-> disciplina;
+    }
+    public function getNota_p1():float{
+        return $this-> nota_p1;
+    }    
+    public function getNota_p2():float{
+        return $this-> nota_p2;
+    }
 }
+
+$info = new Aluno("Fern",2003762,"ADS","matematica",8.5,9.5);
+echo $info;
