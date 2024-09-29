da3862cacd24435edd83c3be9c210c9e0838fa56
takiguchii <alexandre.pupim360@gmail.com>
2024-09-29 01:25:00 -0300

    update 28/09/24

--- POO/p1/2_aluno.php
+++ POO/p1/2_aluno.php
@@ -1,6 +1,6 @@
 <?php 
 
-class aluno{
+class Aluno { 
     private string $nome;
     private int $ra;
     private string $curso;
@@ -9,53 +9,93 @@ class aluno{
     private float $nota_p2;
 
 
-    public function __construct($nome,$ra,$curso,$disciplina,$nota_p1,$nota_p2){
-        $this-> nome = $nome;
-        $this-> ra = $ra;
-        $this-> curso = $curso; 
-        $this-> disciplina = $disciplina;
-        $this-> nota_p1 = $nota_p1;
-        $this-> nota_p2 = $nota_p2;
+    // construct 
+    public function __construct($nome, $ra,$disciplina) {
+        $this->setNome($nome);
+        $this->setRa($ra);
+        $this->setDisciplina($disciplina);
     }
 
-    public function setNome(string $name):string{
+    //sets e gets 
+    public function setNome(string $name) {
         $this->nome = $name;
     }
-    public function setRa(int $ra):int{
+    public function getNome(): string {
+        return $this->nome;
+    }
+    public function setRa(int $ra) {
         $this->ra = $ra;
     }
-    public function setCurso(string $curso):string{
+    public function getRa(): int {
+        return $this->ra;
+    }
+    public function setCurso(string $curso) {
         $this->curso = $curso;
     }
-    public function setDisciplina(string $disciplina):string{
-        $this-> disciplina = $disciplina;
+    public function getCurso(): string {
+        return $this->curso;
     }
-    public function setNota_p1(float $nota_p1):float{
+    public function setDisciplina(string $disciplina) {
+        $this->disciplina = $disciplina;
+    }
+    public function getDisciplina(): string {
+        return $this->disciplina;
+    }
+    public function setNotaP1(float $nota_p1) {
         $this->nota_p1 = $nota_p1;
     }
-    public function setNota_p2(float $nota_p2):float{
+    public function getNotaP1(): float {
+        return $this->nota_p1;
+    }    
+    public function setNotaP2(float $nota_p2) {
         $this->nota_p2 = $nota_p2;
     }
+    public function getNotaP2(): float {
+        return $this->nota_p2;
+    }
+    //funcoes
+    public function calcularMedia(){
+        return($this->nota_p1 + $this->nota_p2) / 2;
+    }
+    public function situacao(){
+        $situacao = "";
+        $media = ($this->nota_p1 + $this->nota_p2) / 2;
+        if($media >= 7 ){
+            $situacao = "aprovado";
+        }elseif($media >=5 and $media < 7){
+            $situação = "exame";
+        }else{
+            $situacao = "reprovado";
+        }
+        return $situacao;
+    }
 
-    public function getNome():string{
-        return $this-> nome;
-    }
-    public function getRa():int{
-        return $this-> ra;
-    }
-    public function getCurso():string{
-        return $this-> curso;
-    }
-    public function getDisciplina():string{
-        return $this-> disciplina;
-    }
-    public function getNota_p1():float{
-        return $this-> nota_p1;
-    }    
-    public function getNota_p2():float{
-        return $this-> nota_p2;
-    }
 }
 
-$info = new Aluno("Fern",2003762,"ADS","matematica",8.5,9.5);
-echo $info;
+echo "<h1> Instância 1 </h1>";
+
+$info = new Aluno("Fern", 2003711, "Matemática");
+$info->setCurso("Medicina");
+$info->setNotap1(10);
+$info->setNotap2(9.5);
+
+echo "Nome: " . $info->getNome() . "<hr>";
+echo "RA: " . $info->getRa() . "<hr>";
+echo "Disciplina: " . $info->getDisciplina() . "<hr>";
+echo "Curso: ", $info->getCurso(), "<hr>";
+echo "Media: ", $info->calcularMedia (), "<hr>";
+echo "situação: ", $info->situacao(), "<hr>";
+
+echo "<h1> Instância 2 <br> </h1>";
+
+$info = new Aluno("Alexandre", 2003762, "Poo");
+$info->setCurso("Analise e Desenvolvimento de Sistemas");
+$info->setNotap1(8);
+$info->setNotap2(7);
+
+echo "Nome: " . $info->getNome() . "<hr>";
+echo "RA: " . $info->getRa() . "<hr>";
+echo "Curso: ", $info->getCurso(), "<hr>";
+echo "Disciplina: " . $info->getDisciplina() . "<hr>";
+echo "Media: ", $info->calcularMedia (), "<hr>";
+echo "situação: ", $info->situacao(), "<hr>";
--- POO/p1/3_cartao.php
+++ POO/p1/3_cartao.php
@@ -5,4 +5,37 @@ class cartao{
     private string $bandeira;
     private float $limite;
     private float $fatura;
+
+    public function __construct($bandeira, $limite){
+        $this->setBandeira($bandeira);
+        $this->setLimite($limite);
+    }
+
+    //sets e gets 
+
+    public function setBanco(){
+
+    }
+    public function getBanco(){
+        
+    }
+    public function setBandeira(){
+        
+    }
+    public function getBandeira(){
+        
+    }
+    public function setLimite(){
+        
+    }
+    public function getLimite(){
+        
+    }
+    public function setFatura(){
+        
+    }
+    public function getFatura(){
+        
+    }
+
 }
