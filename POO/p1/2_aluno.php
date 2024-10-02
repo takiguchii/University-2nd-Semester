ca68d20cf28b2b4d6f1aef58b7e21a07e1b1aae4
takiguchi <alexandre.pupim360@gmail.com>
2024-10-02 19:06:20 -0300

    upload 02/10/24

--- POO/p1/2_aluno.php
+++ POO/p1/2_aluno.php
@@ -1,6 +1,6 @@
 <?php 
 
-class aluno{
+class Aluno { 
     private string $nome;
     private int $ra;
     private string $curso;
@@ -8,54 +8,82 @@ class aluno{
     private float $nota_p1;
     private float $nota_p2;
 
-
-    public function __construct($nome,$ra,$curso,$disciplina,$nota_p1,$nota_p2){
-        $this-> nome = $nome;
-        $this-> ra = $ra;
-        $this-> curso = $curso; 
-        $this-> disciplina = $disciplina;
-        $this-> nota_p1 = $nota_p1;
-        $this-> nota_p2 = $nota_p2;
+    public function __construct($nome, $ra,$disciplina) {
+        $this->setNome($nome);
+        $this->setRa($ra);
+        $this->setDisciplina($disciplina);
     }
-
-    public function setNome(string $name):string{
+    public function setNome(string $name):void{
         $this->nome = $name;
     }
-    public function setRa(int $ra):int{
+    public function getNome(): string {
+        return $this->nome;
+    }
+    public function setRa(int $ra):void{
         $this->ra = $ra;
     }
-    public function setCurso(string $curso):string{
+    public function getRa(): int {
+        return $this->ra;
+    }
+    public function setCurso(string $curso):void{
         $this->curso = $curso;
     }
-    public function setDisciplina(string $disciplina):string{
-        $this-> disciplina = $disciplina;
+    public function getCurso(): string {
+        return $this->curso;
     }
-    public function setNota_p1(float $nota_p1):float{
+    public function setDisciplina(string $disciplina):void {
+        $this->disciplina = $disciplina;
+    }
+    public function getDisciplina(): string {
+        return $this->disciplina;
+    }
+    public function setNotaP1(float $nota_p1):void{
         $this->nota_p1 = $nota_p1;
     }
-    public function setNota_p2(float $nota_p2):float{
+    public function getNotaP1(): float {
+        return $this->nota_p1;
+    }    
+    public function setNotaP2(float $nota_p2):void{
         $this->nota_p2 = $nota_p2;
     }
-
-    public function getNome():string{
-        return $this-> nome;
+    public function getNotaP2(): float {
+        return $this->nota_p2;
     }
-    public function getRa():int{
-        return $this-> ra;
+    public function calcularMedia(){
+        return($this->nota_p1 + $this->nota_p2) / 2;
     }
-    public function getCurso():string{
-        return $this-> curso;
+    public function situacao(){
+        $situacao = "";
+        $media = ($this->nota_p1 + $this->nota_p2) / 2;
+        if($media >= 7 ){
+            $situacao = "aprovado";
+        }elseif($media >=5 and $media < 7){
+            $situação = "exame";
+        }else{
+            $situacao = "reprovado";
+        }
+        return $situacao;
     }
-    public function getDisciplina():string{
-        return $this-> disciplina;
-    }
-    public function getNota_p1():float{
-        return $this-> nota_p1;
-    }    
-    public function getNota_p2():float{
-        return $this-> nota_p2;
+    public function infos(): void {
+        echo "Nome: " . $this->getNome() . "<hr>";
+        echo "RA: " . $this->getRa() . "<hr>";
+        echo "Disciplina: " . $this->getDisciplina() . "<hr>";
+        echo "Curso: " . $this->getCurso() . "<hr>";
+        echo "Média: " . $this->calcularMedia() . "<hr>";
+        echo "Situação: " . $this->situacao() . "<hr>";
     }
 }
 
-$info = new Aluno("Fern",2003762,"ADS","matematica",8.5,9.5);
-echo $info;
+echo "<h1>Instância 1</h1>";
+$info1 = new Aluno("Fern", 2003711, "Matemática");
+$info1->setCurso("Medicina");
+$info1->setNotap1(10);
+$info1->setNotap2(9.5);
+$info1->infos();
+
+echo "<h1>Instância 2</h1>";
+$info2 = new Aluno("Alexandre", 2003762, "Poo");
+$info2->setCurso("Análise e Desenvolvimento de Sistemas");
+$info2->setNotap1(8);
+$info2->setNotap2(7);
+$info2->infos();
--- POO/p1/3_cartao.php
+++ POO/p1/3_cartao.php
@@ -1,8 +1,70 @@
 <?php
 
-class cartao{
+class Cartao{
     private string $banco;
     private string $bandeira;
     private float $limite;
     private float $fatura;
-}
+
+    public function __construct($banco, $bandeira, $limite){
+        $this->setBanco($banco);
+        $this->setBandeira($bandeira);
+        $this->setLimite($limite);
+    }
+
+    public function setBanco(string $banco):void{
+        $this-> banco = $banco;
+    }
+    public function getBanco():string{
+        return $this->banco; 
+    }
+    public function setBandeira(string $bandeira):void{
+        $this-> bandeira = $bandeira;
+    }
+    public function getBandeira():string{
+        return $this->bandeira;
+    }
+    public function setLimite(int $limite):void{
+        $this-> limite = $limite;
+    }
+    public function getLimite():int{
+        return $this->limite;
+    }
+    public function setFatura(float $fatura):void{
+        $this-> fatura = $fatura; 
+    }
+    public function getFatura():float{
+        return $this->fatura; 
+    }
+
+    public function fazerCompra(float $valorCompra) {
+        if($valorCompra > $this->limite) {
+            $valorCompra = False;
+        } else {
+            $this->fatura = $valorCompra;
+            $valorCompra = True;
+        }
+    }
+    public function totalFature($valorCompra) {
+        if ($valorCompra > $this->limite) {
+            return 0; 
+        }
+        return $valorCompra; 
+    }
+}   
+
+echo"<h1>Instancia 1 </h1>";
+$dados = new Cartao("nubank", "Elo", 1500);
+echo "Banco: " . $dados->getBanco() . "<hr>";
+echo "Bandeira: " . $dados->getBandeira() . "<hr>";
+echo "Limite: " . $dados->getLimite() . "<hr>";
+$dados->fazerCompra($valorCompra = 400); 
+echo "Total da Fatura: " . $dados->totalFature($valorCompra) . "<hr>";
+    
+echo"<h1>Instancia 2 </h1>";
+$dados = new Cartao("Bradesco", "Elo", 4500);
+echo "Banco: " . $dados->getBanco() . "<hr>";
+echo "Bandeira: " . $dados->getBandeira() . "<hr>";
+echo "Limite: " . $dados->getLimite() . "<hr>";
+$dados->fazerCompra($valorCompra = 40000); 
+echo "Total da Fatura: " . $dados->totalFature($valorCompra) . "<hr>";
