f565d3697459fc0089eef3d09bd1660f3c432bbb
takiguchi <alexandre.pupim360@gmail.com>
2024-10-20 11:06:23 -0300

    upload sunday 20/10/24

--- POO/conta/contabancaria.php
+++ POO/conta/contabancaria.php
@@ -1,21 +1,49 @@
 <?php 
 
-class ContaBancaria{
+class ContaBancaria {
     private string $titular;
     private float $saldo;
 
-    public function __construct($titular, $saldo ){
+    public function __construct(string $titular, float $saldo) {
         $this->titular = $titular;
         $this->saldo = $saldo;
     }
 
-    public function depositar(){
 
+    public function setTitular(){
+        $this->titular = $titular;
     }
-    public function sacar(){
 
+    public function getTitular(){
+        return $this-> titular;
     }
-    public function getsaldo(){
-        
+
+    public function setSaldo(){
+        $this-> saldo = $saldo;
     }
+
+    public function getSaldo(): float {
+        return $this->saldo;
+    }
+    
+    public function depositar(float $valorDepositado): float {
+        $this->saldo += $valorDepositado;
+        return $this->saldo;
+    }
+    public function sacar(float $valorParaSacar): void {
+        if ($valorParaSacar <= $this->saldo) {
+            $this->saldo -= $valorParaSacar;
+            echo "Saque realizado com sucesso... <br> Valor do saque: {$valorParaSacar}$ <hr>";
+        } else {
+            echo "Saque inválido. Saldo insuficiente. <hr>";
+        }
+    }
+    public function saldo(){
+        echo"Saldo Total: {$this->saldo} <hr>";
+    }
+    public function titular(){
+        echo "<h1>","Titular: {$this->titular} </h1> <hr>";
+    }
+
+
 }
---
+++ POO/conta/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once 'contabancaria.php';
+require_once 'contacorrente.php';
+require_once 'contapoupanca.php';
+
+$conta1 = new ContaBancaria("Pedro", 200.2);
+$valorParaSacar = 100;
+$conta1 ->titular();
+$conta1->sacar($valorParaSacar);
+$conta1 ->saldo();
+
